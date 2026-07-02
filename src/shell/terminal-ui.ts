699ca2d3b0f5b0cf72c8b2cd5660ac7b1f52b134
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2026-07-01 23:33:23 -0300

    feat(terminal): add command autosuggestions and regex-based syntax highlighting

--- src/shell/terminal-ui.ts
+++ src/shell/terminal-ui.ts
@@ -11,6 +11,7 @@ export class TerminalUI {
   private histIdx = -1
   private cwd = '/'
   private _busy = false
+  private suggestion = ''
   private _onCommand: (cmd: string) => void
 
   constructor(container: HTMLElement, onCommand: (cmd: string) => void) {
@@ -94,12 +95,17 @@ export class TerminalUI {
 
     if (this._busy) return
 
+    const termCur = this.cur
+
     // Enter
     if (data === '\r') {
+      // Clear suggestion before submitting
+      this.suggestion = ''
+      this._refreshLine(termCur)
       this.term.write('\r\n')
       const cmd = this.buf.trim()
       if (cmd) {
-        this.history.unshift(cmd)
+        if (this.history[0] !== cmd) this.history.unshift(cmd)
         if (this.history.length > 500) this.history.pop()
         this.histIdx = -1
         this._busy = true
@@ -117,7 +123,7 @@ export class TerminalUI {
       if (this.cur > 0) {
         this.buf = this.buf.slice(0, this.cur - 1) + this.buf.slice(this.cur)
         this.cur--
-        this.term.write('\b \b')
+        this._refreshLine(termCur)
       }
       return
     }
@@ -126,57 +132,111 @@ export class TerminalUI {
     if (data.startsWith('\x1b[')) {
       switch (data) {
         case '\x1b[A': // Up
-          if (this.histIdx < this.history.length - 1) this._setLine(this.history[++this.histIdx])
+          if (this.histIdx < this.history.length - 1) {
+            this.histIdx++
+            this.buf = this.history[this.histIdx]
+            this.cur = this.buf.length
+            this._refreshLine(termCur)
+          }
           return
         case '\x1b[B': // Down
-          if (this.histIdx > 0) this._setLine(this.history[--this.histIdx])
-          else if (this.histIdx === 0) { this.histIdx = -1; this._setLine('') }
+          if (this.histIdx > 0) {
+            this.histIdx--
+            this.buf = this.history[this.histIdx]
+            this.cur = this.buf.length
+            this._refreshLine(termCur)
+          }
+          else if (this.histIdx === 0) { 
+            this.histIdx = -1
+            this.buf = ''
+            this.cur = 0
+            this._refreshLine(termCur)
+          }
           return
         case '\x1b[D': // Left
-          if (this.cur > 0) { this.cur--; this.term.write('\x1b[D') }
+          if (this.cur > 0) { this.cur--; this._refreshLine(termCur) }
           return
         case '\x1b[C': // Right
-          if (this.cur < this.buf.length) { this.cur++; this.term.write('\x1b[C') }
+          if (this.cur < this.buf.length) { this.cur++; this._refreshLine(termCur) }
+          else if (this.suggestion) {
+            this.buf += this.suggestion
+            this.cur = this.buf.length
+            this._refreshLine(termCur)
+          }
           return
         case '\x1b[H': case '\x1b[1~': // Home
-          if (this.cur > 0) { this.term.write(`\x1b[${this.cur}D`); this.cur = 0 }
+          if (this.cur > 0) { this.cur = 0; this._refreshLine(termCur) }
           return
         case '\x1b[F': case '\x1b[4~': // End
-          if (this.cur < this.buf.length) {
-            this.term.write(`\x1b[${this.buf.length - this.cur}C`)
-            this.cur = this.buf.length
+          if (this.suggestion && this.cur === this.buf.length) {
+            this.buf += this.suggestion
           }
+          this.cur = this.buf.length
+          this._refreshLine(termCur)
           return
         case '\x1b[3~': // Delete
           if (this.cur < this.buf.length) {
             this.buf = this.buf.slice(0, this.cur) + this.buf.slice(this.cur + 1)
-            const rest = this.buf.slice(this.cur)
-            this.term.write(rest + ' \x1b[' + (rest.length + 1) + 'D')
+            this._refreshLine(termCur)
           }
           return
         default: return
       }
     }
 
+    // Tab (autocomplete placeholder, just accept suggestion for now)
+    if (data === '\t') {
+      if (this.suggestion) {
+        this.buf += this.suggestion
+        this.cur = this.buf.length
+        this._refreshLine(termCur)
+      }
+      return
+    }
+
     // Printable chars
     if (data.charCodeAt(0) >= 32) {
       this.buf = this.buf.slice(0, this.cur) + data + this.buf.slice(this.cur)
       this.cur += data.length
-      if (this.cur === this.buf.length) {
-        this.term.write(data)
-      } else {
-        const rest = this.buf.slice(this.cur)
-        this.term.write(data + rest + `\x1b[${rest.length}D`)
-      }
+      this._refreshLine(termCur)
     }
   }
 
-  private _setLine(line: string) {
-    const back = this.cur > 0 ? `\x1b[${this.cur}D` : ''
-    const clear = ' '.repeat(this.buf.length)
-    const home = this.buf.length > 0 ? `\x1b[${this.buf.length}D` : ''
-    this.term.write(back + clear + home + line)
-    this.buf = line
-    this.cur = line.length
+  private _refreshLine(termCur: number) {
+    const back = termCur > 0 ? `\x1b[${termCur}D` : ''
+    
+    this.suggestion = ''
+    if (this.buf.length > 0) {
+      const match = this.history.find(h => h.startsWith(this.buf) && h !== this.buf)
+      if (match) this.suggestion = match.slice(this.buf.length)
+    }
+
+    const highlighted = this._highlight(this.buf)
+    const suggestionText = this.suggestion ? `\x1b[38;5;240m${this.suggestion}\x1b[0m` : ''
+    
+    // Move to start, clear to end of line, write highlighted + suggestion
+    this.term.write(back + '\x1b[K' + highlighted + suggestionText)
+    
+    const moveBack = this.buf.length + this.suggestion.length - this.cur
+    if (moveBack > 0) {
+      this.term.write(`\x1b[${moveBack}D`)
+    }
+  }
+
+  private _highlight(text: string) {
+    let isFirstWord = true
+    return text.replace(/(['"].*?['"])|(--?[a-zA-Z0-9-]+)|(\b\d+\b)|(\S+)/g, (match, str, flag, num, word) => {
+      if (str) return `\x1b[38;5;220m${str}\x1b[0m` // yellow
+      if (flag) return `\x1b[38;5;80m${flag}\x1b[0m` // cyan
+      if (num) return `\x1b[38;5;204m${num}\x1b[0m` // magenta
+      if (word) {
+        if (isFirstWord) {
+          isFirstWord = false
+          return `\x1b[38;5;78m${word}\x1b[0m` // green
+        }
+        return word
+      }
+      return match
+    })
   }
 }
